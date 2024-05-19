60b9dee82974baca0af9b2a85328031089c88bf6
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-05-19 07:17:17 +0200

    feat(map): adds map controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
-import Map from "react-map-gl/maplibre";
-import "maplibre-gl/dist/maplibre-gl.css";
-import "./App.css";
+import { useRef } from "react";
+import Map, {
+  MapRef,
+  FullscreenControl,
+  NavigationControl,
+  GeolocateControl,
+  ScaleControl,
+} from "react-map-gl/maplibre";
 
-const mapStyle = `https://api.maptiler.com/maps/streets/style.json?key=${
+import "maplibre-gl/dist/maplibre-gl.css";
+
+const mapStyle = `https://api.maptiler.com/maps/basic/style.json?key=${
   import.meta.env.VITE_APP_MAPTILER_API_KEY
 }`;
 
 function App() {
+  const mapRef = useRef<MapRef | null>(null);
+
   return (
     <Map
+      ref={mapRef}
       initialViewState={{
-        longitude: 0,
-        latitude: 0,
-        zoom: 1,
-      }}
-      style={{
-        position: "absolute",
-        top: 0,
-        right: 0,
-        width: "100vw",
-        height: "100vh",
+        longitude: 5.2913,
+        latitude: 52.1326,
+        zoom: 7.2,
       }}
       mapStyle={mapStyle}
-    />
+    >
+      <FullscreenControl />
+      <NavigationControl />
+      <GeolocateControl />
+      <ScaleControl />
+    </Map>
   );
 }
 
